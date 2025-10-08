52e9cc361cbfda9d5436a2f566b3b18a2537455a
Tamer <daniilszhigulin@yandex.ru>
2025-10-08 20:13:34 +0700

    добавлен класс оружия S-rank

---
+++ Damage_TestConsole/W-Engines_S-rank.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Damage_TestConsole
+{
+    internal class W_Engines_S_rank
+    {
+        //S-Rank
+        //SteelCushion
+        double Wengine_SteelCushion_Main = 684;
+        double Wengine_SteelCushion_Sub_CritRate = 0.24;
+        double[] Wengine_SteelCushion_Pasive_PhysicalDMG = { 0.2, 0.25, 0.3, 0.35, 0.4 };
+        double[] Wengine_SteelCushion_Pasive_DMG = { 0.25, 0.315, 0.38, 0.44, 0.5 };
+
+        //Brimstone
+        double Wengine_Brimstone_Main = 684;
+        double Wengine_Brimstone_Sub_ATK = 0.3;
+        double[] WengineBrimstone__Pasive_ATK_stac_1 = { 0.035, 0.044, 0.052, 0.06, 0.07 };
+        double[] WengineBrimstone__Pasive_ATK_stac_2 = { 0.07, 0.088, 0.104, 0.12, 0.14 };
+        double[] WengineBrimstone__Pasive_ATK_stac_3 = { 0.105, 0.132, 0.156, 0.18, 0.21 };
+        double[] WengineBrimstone__Pasive_ATK_stac_4 = { 0.14, 0.176, 0.208, 0.24, 0.28 };
+        double[] WengineBrimstone__Pasive_ATK_stac_5 = { 0.175, 0.22, 0.26, 0.3, 0.35 };
+        double[] WengineBrimstone__Pasive_ATK_stac_6 = { 0.21, 0.264, 0.312, 0.36, 0.42 };
+        double[] WengineBrimstone__Pasive_ATK_stac_7 = { 0.245, 0.308, 0.364, 0.42, 0.49 };
+        double[] WengineBrimstone__Pasive_ATK_stac_8 = { 0.28, 0.352, 0.416, 0.48, 0.56 };
+
+        //Deep Sea Visitor
+        double Wengine_DeepSeaVisitor_Main = 713;
+        double Wengine_DeepSeaVisitor_Sub_CritRate = 0.24;
+        double[] Wengine_DeepSeaVisitor_Pasive_IceDMG = { 0.25, 0.315, 0.38, 0.445, 0.5 };
+        double[] Wengine_DeepSeaVisitor_Pasive_CritRate_Main = { 0.1, 0.125, 0.15, 0.175, 0.2 };
+        double[] Wengine_DeepSeaVisitor_Pasive_CritRate_Sub_if_IceDng = { 0.1, 0.125, 0.15, 0.175, 0.2 };
+
+        //Zanshin Herb Case
+        double Wengine_ZanshinHerbCase_Main = 713;
+        double Wengine_ZanshinHerbCase_Sub_CritDMG = 0.48;
+        double[] Wengine_ZanshinHerbCase_Pasive_CritRate = { 0.1, 0.115, 0.13, 0.145, 0.16 };
+        double[] Wengine_ZanshinHerbCase_Pasive_DashElectricDMG = { 0.4, 0.46, 0.52, 0.58, 0.64 };
+        double[] Wengine_ZanshinHerbCase_Pasive_CritRate_Sub_if_AnomalyStun = { 0.1, 0.115, 0.13, 0.145, 0.16 };
+
+        //RiotSuppressorMarkVI
+        double Wengine_RiotSuppressorMarkVI_Main = 713;
+        double Wengine_RiotSuppressorMarkVI_Sub_CritDMG = 0.48;
+        double[] Wengine_RiotSuppressorMarkVI_Pasive_CritRate = { 0.15, 0.188, 0.226, 0.264, 0.3 };
+        double[] Wengine_RiotSuppressorMarkVI_Pasive_Basic_Dash_DMG = { 0.35, 0.435, 0.52, 0.605, 0.7 };
+
+        /*double Wengine__Main = ;
+        double Wengine__Sub_ = ;
+        double[] Wengine__Pasive_ = {  } ;*/
+    }
+}
